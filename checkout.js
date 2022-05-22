e4de587c65e7b8c8fc1df127f3e9e6886b9d72a2
vikashkumar2701 <1906645@kiit.ac.in>
2022-05-22 22:01:36 +0530

    order array

--- checkout.js
+++ checkout.js
@@ -1664,7 +1664,8 @@
                                         
 
 
-                                    }
+                                    },
+                                    "orders": this.customer
 
                                 },
                                
